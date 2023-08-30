1ef15ba858b2ec5f394c35e2afd34dffa04a1aec
Matej Matuska <75834032+matejmatuska@users.noreply.github.com>
2023-08-30 18:03:06 +0200

    fixup! change navbar color

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,6 +26,13 @@ intersphinx_mapping = {
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
 
 # -- Options for HTML output
 
